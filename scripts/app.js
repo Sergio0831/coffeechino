4a99d3fd0904f57120f62c458cecaa3a397c0116
Sergio0831 <sergejs.ivcenko@gmail.com>
2023-07-30 11:53:12 +0100

    Created footer

--- scripts/app.js
+++ scripts/app.js
@@ -64,4 +64,112 @@ document.addEventListener('DOMContentLoaded', () => {
 			closeMenu();
 		});
 	});
+
+	// Menu data
+	const menuItems = [
+		{
+			drinks: {
+				sizes: true,
+				categories: {
+					coffee: [
+						{
+							name: 'Espresso',
+							description: 'Espresso machine or coffee maker',
+							price: [2.5, 3],
+						},
+						{
+							name: 'Americano',
+							description: 'Espresso shot',
+							price: [3.2, 3.5],
+						},
+						{
+							name: 'Cappuccino',
+							description: 'Regular, Flavoured',
+							price: [3.5, 3.7],
+						},
+						{
+							name: 'Late',
+							description: 'Regular, Flavoured',
+							price: [3.5, 3.7],
+						},
+					],
+					tea: [
+						{
+							name: 'Black Tea',
+							description: 'Earl Gray, Irish Breakfast',
+							price: [2.7],
+						},
+						{
+							name: 'Green Tea',
+							description: 'Jasmine, Matcha',
+							price: [2.9],
+						},
+					],
+					other: [
+						{
+							name: 'Hot Chocolate',
+							description: 'Marshmallows, cream',
+							price: [3.8, 4.2],
+						},
+						{
+							name: 'Smoothies',
+							description: 'Mixed Berry, Mango-Pineapple, Banana',
+							price: [4.6, 5.1],
+						},
+					],
+				},
+			},
+			food: {
+				sizes: false,
+				categories: {
+					sandwiches: [
+						{
+							name: 'Classic BLT',
+							description: 'Bacon, Lettuce, Tomato',
+							price: [8.5],
+						},
+						{
+							name: 'Turkey and Swiss',
+							description: 'Turkey, Swiss cheese',
+							price: [8.5],
+						},
+						{
+							name: 'Ham and Cheese',
+							description: 'Tomato, Mozzarella, Basil',
+							price: [8.5],
+						},
+						{
+							name: 'Chicken Caesar Wrap',
+							description: 'Crispy chicken, Cucumber, Tomato',
+							price: [8.5],
+						},
+					],
+					salads: [
+						{
+							name: 'Caesar Salad',
+							description: 'Romaine lettuce, Caesar Dressing',
+							price: [9.5],
+						},
+						{
+							name: 'Greek Salad',
+							description: 'Mixed veg, Greek Dressing',
+							price: [10.5],
+						},
+					],
+					pastries: [
+						{
+							name: 'Muffins',
+							description: 'Blueberry, Chocolate Chip, Banana Nut',
+							price: [2.75],
+						},
+						{
+							name: 'Cake Slices',
+							description: 'Carrot, Red Velvet, Chocolate',
+							price: [4.0],
+						},
+					],
+				},
+			},
+		},
+	];
 });
