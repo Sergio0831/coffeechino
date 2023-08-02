e7556ad000eb0ca1c80b8e8503758b4bbdc7623d
Sergio0831 <sergejs.ivcenko@gmail.com>
2023-08-02 11:51:26 +0100

    Created menu section and added functionality to switch between drinks and food menus

--- scripts/app.js
+++ scripts/app.js
@@ -1,73 +1,82 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-	/***** Variables *****/
-	const navBtn = document?.querySelector('.nav__hamburger');
-	const navMenu = document?.querySelector('.nav');
-	const navLinks = document?.querySelectorAll('.nav__item a');
-	const bodyEl = document.body;
+	/***** Function for navigation menu and links *****/
+	(function () {
+		/***** Variables *****/
+		const navBtn = document?.querySelector('.nav__hamburger');
+		const navMenu = document?.querySelector('.nav');
+		const navLinks = document?.querySelectorAll('.nav__item a');
+		const bodyEl = document.body;
 
-	/***** Functions *****/
+		/***** Functions *****/
 
-	// Add or remove scroll based on menu is open or closed
-	const toggleScroll = () => {
-		if (navMenu.classList.contains('nav--active')) {
-			bodyEl.classList.add('dis-scroll');
-		} else {
-			bodyEl.classList.remove('dis-scroll');
-		}
-	};
+		// Add or remove scroll based on menu is open or closed
+		const toggleScroll = () => {
+			if (navMenu.classList.contains('nav--active')) {
+				bodyEl.classList.add('dis-scroll');
+			} else {
+				bodyEl.classList.remove('dis-scroll');
+			}
+		};
 
-	// Toggle nav menu on button click
-	const toggleMenu = () => {
-		navBtn?.classList.toggle('burger--active');
-		navMenu?.classList.toggle('nav--active');
-		navBtn?.setAttribute(
-			'aria-expanded',
-			navMenu.classList.contains('nav--active'),
-		);
-		navBtn.setAttribute(
-			'aria-label',
-			navMenu.classList.contains('nav--active') ? 'Close menu' : 'Open menu',
-		);
-		toggleScroll();
-	};
+		// Toggle nav menu on button click
+		const toggleMenu = () => {
+			navBtn?.classList.toggle('burger--active');
+			navMenu?.classList.toggle('nav--active');
+			navBtn?.setAttribute(
+				'aria-expanded',
+				navMenu.classList.contains('nav--active'),
+			);
+			navBtn.setAttribute(
+				'aria-label',
+				navMenu.classList.contains('nav--active') ? 'Close menu' : 'Open menu',
+			);
+			toggleScroll();
+		};
 
-	// Close menu function
-	const closeMenu = () => {
-		navBtn?.classList.remove('burger--active');
-		navMenu?.classList.remove('nav--active');
-		navBtn?.setAttribute('aria-expanded', 'false');
-		navBtn?.setAttribute('aria-label', 'Open menu');
-		toggleScroll();
-	};
+		// Close menu function
+		const closeMenu = () => {
+			navBtn?.classList.remove('burger--active');
+			navMenu?.classList.remove('nav--active');
+			navBtn?.setAttribute('aria-expanded', 'false');
+			navBtn?.setAttribute('aria-label', 'Open menu');
+			toggleScroll();
+		};
 
-	/***** Event Listeners *****/
+		/***** Event Listeners *****/
 
-	// Event listener to open, close menu
-	navBtn?.addEventListener('click', (event) => {
-		event.stopPropagation();
-		toggleMenu();
-	});
-
-	// Event listener to close the menu when clicked outside
-	document.addEventListener('click', (event) => {
-		if (!navMenu?.contains(event.target) && event.target !== navBtn) {
-			closeMenu();
-		}
-	});
-
-	// Event listener to close the menu when menu link is clicked
-	navLinks?.forEach((link) => {
-		link.addEventListener('click', (event) => {
+		// Event listener to open, close menu
+		navBtn?.addEventListener('click', (event) => {
 			event.stopPropagation();
-			closeMenu();
+			toggleMenu();
 		});
-	});
 
-	// Menu data
-	const menuItems = [
-		{
+		// Event listener to close the menu when clicked outside
+		document.addEventListener('click', (event) => {
+			if (!navMenu?.contains(event.target) && event.target !== navBtn) {
+				closeMenu();
+			}
+		});
+
+		// Event listener to close the menu when menu link is clicked
+		navLinks?.forEach((link) => {
+			link.addEventListener('click', (event) => {
+				event.stopPropagation();
+				closeMenu();
+			});
+		});
+	})();
+
+	/***** Function for drinks and food menu *****/
+	(function () {
+		/***** Variables *****/
+		const menuBtns = document.querySelectorAll('.menu__btn');
+		let drinksMenuGenerated = true;
+		let foodMenuGenerated = false;
+
+		// Drinks and food menu data
+		const menuItems = {
 			drinks: {
 				sizes: true,
 				categories: {
@@ -170,6 +179,106 @@ document.addEventListener('DOMContentLoaded', () => {
 					],
 				},
 			},
-		},
-	];
+		};
+
+		/***** Functions *****/
+
+		// Menu category item HTML template
+		const generateMenuCategoryItem = (categoryData) => {
+			const { name, description, price } = categoryData;
+
+			const html = `
+		<div class='category__item'>
+			<div class='category__item-content'>
+				<h4 class='heading-4'>${name}</h4>
+				<p>${description}</p>
+			</div>	
+			<div class='category__item-prices'>
+				${price.map((item) => `<span>&euro;${item.toFixed(2)}</span>`).join(' ')}
+			</div>
+		</div>
+		`;
+			return html;
+		};
+
+		// Menu category HTML template
+		const generateMenuCategory = (categories) => {
+			let html = ``;
+
+			for (const category in categories) {
+				const items = categories[category];
+				html += `
+				<div class='category'>
+					<h3 class='heading-3 text-center'>
+						${category.charAt(0).toUpperCase() + category.slice(1)}
+					</h3>
+					${items.map((item) => generateMenuCategoryItem(item)).join(' ')}
+				</div>
+			`;
+			}
+
+			return html;
+		};
+
+		// Menu HTML template
+		const generateMenu = (menuItem) => {
+			const menuCard = document.querySelector('.menu__card');
+
+			// Remove previous generated menu
+			menuCard.innerHTML = '';
+
+			let html = `
+				${
+					menuItem.sizes
+						? `<div class='sizes'>
+					<span>Regular</span>
+					<span>Large</span>
+				</div>`
+						: ''
+				}
+				${generateMenuCategory(menuItem.categories)}
+			`;
+
+			return menuCard.insertAdjacentHTML('afterbegin', html);
+		};
+
+		// Show drinks menu by default
+		generateMenu(menuItems['drinks']);
+
+		// Show food or drinks menu on button click
+		const showMenuOnClick = (e) => {
+			e.stopPropagation();
+			const target = e.target;
+
+			// Get the aria-label attribute of the button was clicked
+			const ariaLabel = target.getAttribute('aria-label');
+
+			// Remove active class from all buttons
+			menuBtns.forEach((btn) => btn.classList.remove('btn--active'));
+
+			// Add active class to the clicked button
+			target.classList.add('btn--active');
+
+			// Check if the aria-label exist and show menu
+			if (ariaLabel) {
+				// Check which menu to generate based on the clicked button
+				if (ariaLabel.toLowerCase() === 'drinks') {
+					if (!drinksMenuGenerated) {
+						generateMenu(menuItems[ariaLabel.toLowerCase()]);
+						drinksMenuGenerated = true;
+						foodMenuGenerated = false;
+					}
+				} else if (ariaLabel.toLowerCase() === 'food') {
+					if (!foodMenuGenerated) {
+						generateMenu(menuItems[ariaLabel.toLowerCase()]);
+						drinksMenuGenerated = false;
+						foodMenuGenerated = true;
+					}
+				}
+			}
+		};
+
+		/***** Event listeners *****/
+		menuBtns.forEach((btn) => btn.addEventListener('click', showMenuOnClick));
+	})();
 });
